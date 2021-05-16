7ff359387537b9cd31f325ad1a57b5ab11879631
Fasil <fasil.shaikh@graymatrix.com>
2021-05-16 13:24:05 +0530

    command line arguments added

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 module.exports.strToBin = (text) => {
     try{
-        var pad = "00000000";
+        let pad = "00000000";
         return unescape(encodeURIComponent(text))
             .split('').map(function(txt) {
-                var binary = txt.charCodeAt(0).toString(2);
+                let binary = txt.charCodeAt(0).toString(2);
                 return pad.slice(binary.length)+binary;
             }).join(' ');
     }catch(e){
@@ -13,11 +13,11 @@ module.exports.strToBin = (text) => {
 
 module.exports.binToStr = (text, delimiter=" ") => {
     try{
-        var stringeq = []
+        let stringeq = []
         text.split(delimiter).forEach(ele => {
-            var asciiCounter = 0
+            let asciiCounter = 0
             ele = ele.split("").reverse().join("");
-            for(var i = 7; i >= 0; i--){
+            for(let i = 7; i >= 0; i--){
                 if(ele.split("")[i] && ele.split("")[i]==1){
                     asciiCounter += Math.pow(((i==0) ? 1 : 2), ((ele.split("")[i] && ele.split("")[i]==1) ? i : 0))
                 }
